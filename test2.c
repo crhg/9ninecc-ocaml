dde53142544342f8433d0387287bac0148af14f4
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-01 09:57:22 +0900

    コミット忘れ

---
+++ test2.c
@@ -0,0 +1,23 @@
+int add(int x, int y) {
+    return x + y;
+}
+
+int add8(
+        int x1,
+        int x2,
+        int x3,
+        int x4,
+        int x5,
+        int x6,
+        int x7,
+        int x8
+    ) {
+    return x1
+        + x2
+        + x3
+        + x4
+        + x5
+        + x6
+        + x7
+        + x8;
+}

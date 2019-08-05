36f8bf02c2c2835e7ca45847ff36157799f09b7c
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-06 06:00:42 +0900

    type_check.mliから公開する必要のない値を削除

--- type_check.mli
+++ type_check.mli
@@ -1,8 +1,5 @@
 val prepare_func : (Type.t * string) list -> Ast.stmt -> unit
 val allocate_stmt : Ast.stmt -> unit
 val prepare_init : Ast.init -> unit
-val assign_type_plane : Ast.expr -> Type.t
-val normalize_type : Type.t -> Type.t
 val assign_type : Ast.expr -> Type.t
-val find_type : Ast.expr -> Type.t
 val type_and_var : Type.t -> Ast.declarator -> Type.t * string

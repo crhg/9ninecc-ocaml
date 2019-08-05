97111e5f9e059d47fa2e480c9adc63e58d1d6830
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-06 05:39:39 +0900

    env.mliから公開する必要のない値を削除

--- env.mli
+++ env.mli
@@ -1,47 +1,3 @@
-module Env :
-  sig
-    type key = String.t
-    type 'a t = 'a Map.Make(String).t
-    val empty : 'a t
-    val is_empty : 'a t -> bool
-    val mem : key -> 'a t -> bool
-    val add : key -> 'a -> 'a t -> 'a t
-    val update : key -> ('a option -> 'a option) -> 'a t -> 'a t
-    val singleton : key -> 'a -> 'a t
-    val remove : key -> 'a t -> 'a t
-    val merge :
-      (key -> 'a option -> 'b option -> 'c option) -> 'a t -> 'b t -> 'c t
-    val union : (key -> 'a -> 'a -> 'a option) -> 'a t -> 'a t -> 'a t
-    val compare : ('a -> 'a -> int) -> 'a t -> 'a t -> int
-    val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
-    val iter : (key -> 'a -> unit) -> 'a t -> unit
-    val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
-    val for_all : (key -> 'a -> bool) -> 'a t -> bool
-    val exists : (key -> 'a -> bool) -> 'a t -> bool
-    val filter : (key -> 'a -> bool) -> 'a t -> 'a t
-    val partition : (key -> 'a -> bool) -> 'a t -> 'a t * 'a t
-    val cardinal : 'a t -> int
-    val bindings : 'a t -> (key * 'a) list
-    val min_binding : 'a t -> key * 'a
-    val min_binding_opt : 'a t -> (key * 'a) option
-    val max_binding : 'a t -> key * 'a
-    val max_binding_opt : 'a t -> (key * 'a) option
-    val choose : 'a t -> key * 'a
-    val choose_opt : 'a t -> (key * 'a) option
-    val split : key -> 'a t -> 'a t * 'a option * 'a t
-    val find : key -> 'a t -> 'a
-    val find_opt : key -> 'a t -> 'a option
-    val find_first : (key -> bool) -> 'a t -> key * 'a
-    val find_first_opt : (key -> bool) -> 'a t -> (key * 'a) option
-    val find_last : (key -> bool) -> 'a t -> key * 'a
-    val find_last_opt : (key -> bool) -> 'a t -> (key * 'a) option
-    val map : ('a -> 'b) -> 'a t -> 'b t
-    val mapi : (key -> 'a -> 'b) -> 'a t -> 'b t
-    val to_seq : 'a t -> (key * 'a) Seq.t
-    val to_seq_from : key -> 'a t -> (key * 'a) Seq.t
-    val add_seq : (key * 'a) Seq.t -> 'a t -> 'a t
-    val of_seq : (key * 'a) Seq.t -> 'a t
-  end
 type entry =
     DummyEntry
   | LocalVar of Type.t * int
@@ -49,14 +5,11 @@ type entry =
 val pp_entry :
   Ppx_deriving_runtime.Format.formatter -> entry -> Ppx_deriving_runtime.unit
 val show_entry : entry -> Ppx_deriving_runtime.string
-val local_env : entry Env.t ref
-val global_env : entry Env.t ref
-val offset : int ref
 val reset : 'a -> unit
 exception DuplicatedLocal of Type.t * string
-val register_local_var : Type.t -> Env.key -> unit
+val register_local_var : Type.t -> string -> unit
 exception DuplicatedGlobal of Type.t * string
-val register_global_var : Type.t -> Env.key -> unit
+val register_global_var : Type.t -> string -> unit
 val local_var_size : 'a -> int
-val get_entry : Env.key -> entry
+val get_entry : string -> entry
 val entry_type : entry -> Type.t

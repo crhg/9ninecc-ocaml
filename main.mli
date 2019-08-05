bd8ddae48359329a53a5c3ab74723e48110857f1
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-06 05:54:11 +0900

    main.mliは不要なので削除(他に提供するインタフェースはない)

--- main.mli
+++
@@ -1,7 +0,0 @@
-val get_line : string -> int -> string * int * int
-val repeat : int -> string -> string
-val compile : string -> string -> unit
-val compile_file : string -> unit
-val verbose : bool ref
-val filenames : string list ref
-val spec : (string * Arg.spec * string) list

3427da3f016a452aa600ef1abaa2e4659df3a523
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-05 16:53:39 +0900

    代入式のastを作ってコード生成する方法

--- init_local.ml
+++ init_local.ml
@@ -1,12 +1,13 @@
 let rec gen ty d init =
     let ty, name = Type_check.type_and_var ty d in
+    let ident = make_ident ty name d.loc in
     match ty with
     | Type.Char
     | Type.Int
     | Type.Ptr _ ->
-        gen_scalar_init ty name init
+        gen_scalar_init ty ident init
     | Type.Array _ ->
-        gen_array_init ty name init
+        gen_array_init ty ident init
     | _ -> raise (Misc.Error_at("cannot initialize", init.Ast.loc))
 
 and gen_scalar_init ty name init = Ast.(match init.exp with
@@ -16,13 +17,34 @@ and gen_scalar_init ty name init = Ast.(match init.exp with
     | _ -> raise(Misc.Error_at("invalid initializer", init.loc))
 )
 
-and gen_assign ty name expr =
-    Gen_expr.gen_lval_name name;
-    Gen_expr.gen_expr expr;
-    Stack.pop "rdi";
-    Stack.pop "rax";
-    Gen_misc.store ty "[rax]" "rdi";
-    Stack.push "rdi"
+and gen_assign ident expr =
+    let assign = gen_assign ident expr in
+    Gen_expr.gen_expr assign;
+
+    (* Gen_expr.gen_lval_name name; *)
+    (* Gen_expr.gen_expr expr; *)
+    (* Stack.pop "rdi"; *)
+    (* Stack.pop "rax"; *)
+    (* Gen_misc.store ty "[rax]" "rdi"; *)
+    (* Stack.push "rdi" *)
 
 and gen_array_init ty name init =
     failwith "not implemented"
+
+and make_ident name loc =
+    Ast.({
+        exp = {
+            e = Ident (name, ref Env.DummyEntry);
+            ty = None
+        };
+        loc = loc
+    })
+
+and make_assign lhs rhs =
+    Ast.({
+        exp = {
+            e = Assign (lhs, rhs);
+            ty = None
+        };
+        loc = lhs.loc
+    })

24f55f8d53715897a2fca58ceda11363d1e2f920
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-05 17:02:45 +0900

    代入式のastを作ってコード生成する方法(やり直し)

--- init_local.ml
+++ init_local.ml
@@ -1,27 +1,27 @@
 let rec gen ty d init =
     let ty, name = Type_check.type_and_var ty d in
-    let ident = make_ident ty name d.loc in
+    let ident = make_ident name d.loc in
     match ty with
     | Type.Char
     | Type.Int
     | Type.Ptr _ ->
-        gen_scalar_init ty ident init
+        gen_scalar_init ident init
     | Type.Array _ ->
         gen_array_init ty ident init
     | _ -> raise (Misc.Error_at("cannot initialize", init.Ast.loc))
 
-and gen_scalar_init ty name init = Ast.(match init.exp with
+and gen_scalar_init name init = Ast.(match init.exp with
     | ExprInitializer expr
     | ListInitializer [{exp=ExprInitializer expr}] ->
-        gen_assign ty name expr
+        gen_assign name expr
     | _ -> raise(Misc.Error_at("invalid initializer", init.loc))
 )
 
 and gen_assign ident expr =
-    let assign = gen_assign ident expr in
-    Gen_expr.gen_expr assign;
+    let assign = make_assign ident expr in
+    ignore(Gen_expr.gen_expr assign)
 
-and gen_array_init ty name init =
+and gen_array_init ty ident init =
     failwith "not implemented"
 
 and make_ident name loc =

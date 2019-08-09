cf295fee14a9baeb3ae8edb0c652543a21e3ec73
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-09 21:49:38 +0900

    struct型のみ

--- type.ml
+++ type.ml
@@ -4,6 +4,17 @@ type t =
 | Ptr of t
 | Array of t * int option
 | Function of t * (string * t) list (* 戻り値とパラメタ *)
+| Struct of st_un
+
+and field = { field_name : string; field_type: t; field_offset: int }
+
+(* 構造体・共用体の型の共通の中身 *)
+and st_un_exp = {
+    fields: field list;
+    size: int;
+    alignment: int
+}
+and st_un = { mutable exp: st_un_exp option }
 [@@deriving show]
 
 let rec get_size ty = match ty with
@@ -11,6 +22,7 @@ let rec get_size ty = match ty with
 | Char -> 1
 | Ptr _ -> 8
 | Array (t, Some n) -> get_size t * n
+| Struct {exp=Some{size=size}} -> size
 | _ -> failwith @@ "size?: " ^ show ty
 
 and get_alignment ty = match ty with
@@ -18,12 +30,14 @@ and get_alignment ty = match ty with
 | Char -> 1
 | Ptr _ -> 8
 | Array (t, _) -> get_alignment t
+| Struct {exp=Some{alignment=alignment}} -> alignment
 | _ -> failwith @@ "alignment?: " ^ show ty
 
 and is_complete_type ty = match ty with
 | Int
 | Char
-| Ptr _ ->
+| Ptr _
+| Struct {exp=Some _} ->
     true
 | Array (t, Some _) ->
     is_complete_type t
--- type.mli
+++ type.mli
@@ -4,9 +4,23 @@ type t =
   | Ptr of t
   | Array of t * int option
   | Function of t * (string * t) list
+  | Struct of st_un
+and field = { field_name : string; field_type : t; field_offset : int; }
+and st_un_exp = { fields : field list; size : int; alignment : int; }
+and st_un = { mutable exp : st_un_exp option; }
 val pp :
   Ppx_deriving_runtime.Format.formatter -> t -> Ppx_deriving_runtime.unit
 val show : t -> Ppx_deriving_runtime.string
+val pp_field :
+  Ppx_deriving_runtime.Format.formatter -> field -> Ppx_deriving_runtime.unit
+val show_field : field -> Ppx_deriving_runtime.string
+val pp_st_un_exp :
+  Ppx_deriving_runtime.Format.formatter ->
+  st_un_exp -> Ppx_deriving_runtime.unit
+val show_st_un_exp : st_un_exp -> Ppx_deriving_runtime.string
+val pp_st_un :
+  Ppx_deriving_runtime.Format.formatter -> st_un -> Ppx_deriving_runtime.unit
+val show_st_un : st_un -> Ppx_deriving_runtime.string
 val get_size : t -> int
 val get_alignment : t -> int
 val is_complete_type : t -> bool

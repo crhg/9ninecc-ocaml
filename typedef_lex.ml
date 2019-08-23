1e5a388a9695fc05f59eee715560c37fd2f56ea6
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-23 11:46:34 +0900

    typedefの;の後にDUMMYを入れる処理と{の後,}の前にDUMMYを入れる処理を分離

--- typedef_lex.ml
+++ typedef_lex.ml
@@ -1,9 +1,19 @@
 (* lexerのラッパ *)
 open Token
 
-let typedef_found = ref None
-let level = ref 0
-let buf = ref []
+type typedef_status = {
+    found: int option;
+    level: int;
+    buf: Token.token list
+}
+
+let typedef_status = ref {
+    found = None;
+    level = 0;
+    buf = []
+}
+
+let brace_buf = ref []
 
 let rec token lexbuf =
     let t = token' lexbuf in
@@ -15,6 +25,7 @@ and token' lexbuf =
         Lexer.token
         |> typedef_id_wrapper
         |> typedef_hack_wrapper
+        |> brace_wrapper
     )
 
 and typedef_id_wrapper get_token lexbuf =
@@ -24,38 +35,55 @@ and typedef_id_wrapper get_token lexbuf =
 
 (*  typedefの次の{}外の;の次にint;を出力するhack *)
 and typedef_hack_wrapper get_token lexbuf =
-    match !buf with
+    match !typedef_status.buf with
     | token::rest ->
-        buf := rest;
+        typedef_status := { !typedef_status with buf = rest };
         token
-    | [] -> typedef_hack @@ get_token lexbuf
+    | [] ->
+        let token = get_token lexbuf in
+        typedef_status := next_typedef_status !typedef_status token;
+        token
 
-and typedef_hack token =
+and next_typedef_status status token =
     match token with
     | TYPEDEF ->
-        typedef_found := Some !level;
-        TYPEDEF
+        { status with found = Some status.level }
     | SEMI ->
-        (match !typedef_found with
-        | Some lv when lv = !level ->
-            buf := INT :: SEMI :: !buf;
-            typedef_found := None
-        | _ -> ()
-        );
-        SEMI
+        (match status with
+        | { found = Some lv; level = level; _ } when lv = level ->
+            { status with 
+                buf = INT :: SEMI :: status.buf;
+                found = None
+            }
+        | _ ->
+            status
+        )
     | LBRACE ->
-        buf := DUMMY :: !buf;
-        level := !level + 1;
-        LBRACE
+        { status with level = status.level + 1 }
     | RBRACE ->
-        level := !level - 1;
-        buf := RBRACE :: !buf;
-        DUMMY
+        { status with level = status.level - 1 }
     | _ ->
-        token
+        status
 
 and make_token name = 
     if Typedef_env.mem name then
         TYPEDEF_ID name
     else
         IDENT name
+
+and brace_wrapper get_token lexbuf =
+    match !brace_buf with
+    | token::rest ->
+        brace_buf := rest;
+        token
+    | [] ->
+        (match get_token lexbuf with
+        | LBRACE ->
+            brace_buf := DUMMY :: !brace_buf;
+            LBRACE
+        | RBRACE ->
+            brace_buf := RBRACE :: !brace_buf;
+            DUMMY
+        | token ->
+            token
+        )

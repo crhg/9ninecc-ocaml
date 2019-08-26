21f1ffe7096c0d9ff2b60d9f175ff958b1692421
Manabu Matsui <manabu.matsui@gmail.com>
2019-08-26 21:17:10 +0900

    typedefだけのためのものでなくなったのでTypedef_lexをLexにリネーム

--- main.ml
+++ main.ml
@@ -8,7 +8,7 @@ let preprocess filename source =
     )
 
 let compile filename source =
-    Compiler.process filename source Parser.translation_unit Typedef_lex.token (fun ast ->
+    Compiler.process filename source Parser.translation_unit Lex.token (fun ast ->
         Type_check.check ast;
         Gen.gen ast
     )
